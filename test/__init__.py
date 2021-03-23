b57f71035f85c2795dd176964dcb41ee2452b74e
ennggwx <wxiangkuner@163.com>
2021-03-23 17:44:14 +0800

    Firse test commit

---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+print(tf.__version__)
+gpu_ok = tf.config.list_physical_devices('GPU')
+print(gpu_ok)
+print("GPUs: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+
+
